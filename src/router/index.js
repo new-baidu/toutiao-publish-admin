f37a5bbc41e141bd53780a25834a6d7e796fbc51
[root] <1627930319@qq.com>
2020-09-14 23:57:21 +0800

    登录跳转到主页面

--- src/router/index.js
+++ src/router/index.js
@@ -2,6 +2,10 @@ import Vue from 'vue'
 import VueRouter from 'vue-router'
 // 引入表单验证页面
 import Login from '@/views/login/index.vue'
+// 引入首页页面
+import Layout from '@/views/login/layout.vue'
+// 子页面
+import Home from '@/views/login/home.vue'
 
 Vue.use(VueRouter)
 
@@ -10,6 +14,18 @@ const routes = [
     path: '/login',
     name: Login,
     component: Login
+  },
+  {
+    path: '/layout',
+    name: Layout,
+    component: Layout,
+    children: [
+      {
+        path: '',
+        name: 'Home',
+        component: Home
+      }
+    ]
   }
 ]
 export default new VueRouter({
--- src/views/login/index.vue
+++ src/views/login/index.vue
@@ -17,7 +17,7 @@
 
             <!-- 单选按钮区域 -->
             <el-form-item>
-                <el-checkbox v-model="checked">我已阅读并同意用户协议和隐私条款</el-checkbox>
+                <el-checkbox v-model="checked" >我已阅读并同意用户协议和隐私条款</el-checkbox>
             </el-form-item>
 
             <!-- 验证区域 -->
@@ -27,6 +27,84 @@
         </el-form>
     </div>
 </template>
+
+<script>
+import { login } from '@/api/user.js'
+// 引入首页
+import '@/views/login/layout.vue'
+
+export default {
+  data () {
+    return {
+      user: {
+        mobile: '',
+        code: ''
+      },
+      checked: false,
+      loginLoading: false,
+      loginFormRules: {
+        mobile: [
+          { required: true, message: '请输入手机号', trigger: 'blur' },
+          { min: 11, max: 11, message: '长度在 11 个数字', trigger: 'blur' }
+        ],
+        code: [
+          { required: true, message: '请输验证码', trigger: 'blur' },
+          { pattern: /^\d{6}$/, message: '请输入正确的验证码格式', trigger: 'blur' }
+        ],
+        agree: [
+          {
+            vlidator: (rule, vlaue, callback) => {
+              if (vlaue) {
+                // 验证通过
+                callback()
+                // 跳转到首页
+                this.$router.push({
+                  name: 'home'
+                })
+              } else {
+                // 登录失败
+                callback(new Error('请同意用户协议'))
+              }
+            },
+            // 请勾选用户协议
+            trigger: 'blur'
+          }
+        ]
+      }
+    }
+  },
+  methods: {
+    onLogin: function () {
+      // 点击开启loginLoading等待
+      this.loginLoading = true
+
+      // 接口
+      login(this.user).then(res => {
+        console.log(res)
+
+        // 登陆成功消息提示
+        this.$message({
+          message: '登录成功',
+          type: 'success'
+        })
+
+        // 登录成功跳转主页
+        this.$router.push('/layout')
+        // 点击后关闭loginLoading等待
+        this.loginLoading = false
+      }).catch(err => {
+        console.log('登陆失败', err)
+
+        // 登陆失败消息提示
+        this.$message.error('登陆失败，你的手机号或验证码输入错误')
+
+        // 点击后关闭loginLoading等待
+        this.loginLoading = false
+      })
+    }
+  }
+}
+</script>
 
 <style lang="less">
 .login_container{
@@ -46,10 +124,11 @@
         min-width: 300px;
         background-color: #ffffff;
         .login_logo{
-            margin-bottom: 20px;
+            margin-bottom: 10px;
             width: 300px;
             height: 47px;
             background: url(./logo_index.png) no-repeat;
+            background-position: center;
         }
         .login_form_button{
             width: 100%;
@@ -57,63 +136,3 @@
     }
 }
 </style>
-
-<script>
-import request from '@/utils/request.js'
-
-export default {
-  data () {
-    return {
-      user: {
-        mobile: '',
-        code: ''
-      },
-      checked: false,
-      loginLoading: false,
-      loginFormRules: {
-        mobile: [
-          { required: true, message: '请输入手机号', trigger: 'blur' },
-          { min: 11, max: 11, message: '长度在 11 个数字', trigger: 'blur' }
-        ],
-        code: [
-          { required: true, message: '请输验证码', trigger: 'blur' },
-          { min: 4, max: 6, message: '长度在 4 到  6个字符', trigger: 'blur' }
-        ]
-      }
-    }
-  },
-  methods: {
-    onLogin: function () {
-      const user = this.user
-
-      // 点击开启loginLoading等待
-      this.loginLoading = true
-      request({
-        method: 'POST',
-        url: '/mp/v1_0/authorizations',
-        // data用来设置请求体
-        data: user
-      }).then(res => {
-        console.log(res)
-
-        // 登陆成功消息提示
-        this.$message({
-          message: '登录成功',
-          type: 'success'
-        })
-
-        // 点击后关闭loginLoading等待
-        this.loginLoading = false
-      }).catch(err => {
-        console.log('登陆失败', err)
-
-        // 登陆失败消息提示
-        this.$message.error('登陆失败，你的手机号或验证码输入错误')
-
-        // 点击后关闭loginLoading等待
-        this.loginLoading = false
-      })
-    }
-  }
-}
-</script>
