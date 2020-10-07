8d54c0146cab7bb824db7b45a6f0dd81de55255c
月赛 <1627930319@qq.com>
2020-10-07 21:08:42 +0800

    素材管理-展示素材管理

--- src/api/image.js
+++ src/api/image.js
@@ -12,3 +12,12 @@ export const uploadImage = data => {
     data
   })
 }
+
+// 获取素材列表
+export const getImage = params => {
+  return request({
+    method: 'GET',
+    url: '/mp/v1_0/user/images',
+    params
+  })
+}
--- src/views/image/index.vue
+++ src/views/image/index.vue
@@ -1,22 +1,66 @@
 <template>
   <div class="image-content">
-    素材管理
+    <el-card class="box-card">
+      <div slot="header" class="clearfix">
+        <!-- 面包屑 -->
+        <el-breadcrumb separator="/">
+          <el-breadcrumb-item :to="{ path: '/' }">首页</el-breadcrumb-item>
+          <el-breadcrumb-item>素材管理</el-breadcrumb-item>
+        </el-breadcrumb>
+        <!--/ 面包屑 -->
+      </div>
+
+      <!-- 按钮组 -->
+      <div>
+        <el-radio-group v-model="radio1" size="mini">
+          <el-radio-button label="全部"></el-radio-button>
+          <el-radio-button label="收藏"></el-radio-button>
+        </el-radio-group>
+      </div>
+      <!--/ 按钮组 -->
+
+      <!-- 素材列表 -->
+      <el-row :gutter="20">
+        <el-col :lg="4" :sm="6" :md="4" v-for="(img, index) in images" :key="index">
+          <el-image
+            style="height: 100px"
+            :src="img.url"
+            fit="cover"
+          ></el-image
+        ></el-col>
+      </el-row>
+      <!--/ 素材列表 -->
+    </el-card>
   </div>
 </template>
 
 <script>
+import { getImage } from '@/api/image'
+
 export default {
   name: 'ImageIndex',
   components: {},
   props: {},
   data () {
-    return {}
+    return {
+      radio1: '全部',
+      radio2: '收藏',
+      images: [] // 素材列表
+    }
   },
   computed: {},
   watch: {},
-  created () { },
+  created () {
+    this.loadImages()
+  },
   mounted () { },
-  methods: {}
+  methods: {
+    loadImages () {
+      getImage().then(res => {
+        this.images = res.data.data.results
+      })
+    }
+  }
 }
 </script>
 
