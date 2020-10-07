e30915276dacecf724d6605ddd67efee68deef85
月赛 <1627930319@qq.com>
2020-10-07 23:11:37 +0800

    素材管理-上传素材-交互处理

--- src/views/image/index.vue
+++ src/views/image/index.vue
@@ -16,20 +16,54 @@
           <el-radio-button :label="false">全部</el-radio-button>
           <el-radio-button :label="true">收藏</el-radio-button>
         </el-radio-group>
+        <el-button
+          class="action-head"
+          style="float: right; justify-content: space-between"
+          type="primary"
+          size="mini"
+          @click="dialogVisible = true"
+          >添加素材</el-button
+        >
       </div>
       <!--/ 按钮组 -->
 
+      <!-- 添加素材 -->
+      <el-dialog title="上传素材" :visible.sync="dialogVisible" width="40%">
+        <el-upload
+          class="upload-demo"
+          drag
+          action="https://jsonplaceholder.typicode.com/posts/"
+          multiple
+        >
+          <i class="el-icon-upload"></i>
+          <div class="el-upload__text">将文件拖到此处，或<em>点击上传</em></div>
+          <div class="el-upload__tip" slot="tip">
+            只能上传jpg/png文件，且不超过500kb
+          </div>
+        </el-upload>
+      </el-dialog>
+      <!--/ 添加素材 -->
+
       <!-- 素材列表 -->
-      <el-row :gutter="20">
-        <el-col :lg="4" :sm="6" :md="4" v-for="(img, index) in images" :key="index">
-          <el-image
-            style="height: 100px"
-            :src="img.url"
-            fit="cover"
-          ></el-image
-        ></el-col>
+      <el-row :gutter="5">
+        <el-col
+          :xs="12"
+          :lg="4"
+          :sm="6"
+          :md="6"
+          v-for="(img, index) in images"
+          :key="index"
+        >
+          <el-image style="height: 100px" :src="img.url" fit="cover"></el-image>
+        </el-col>
       </el-row>
       <!--/ 素材列表 -->
+
+      <!-- 分页 -->
+      <div class="block">
+        <el-pagination layout="prev, pager, next"> </el-pagination>
+      </div>
+      <!--/ 分页 -->
     </el-card>
   </div>
 </template>
@@ -44,7 +78,8 @@ export default {
   data () {
     return {
       radio1: '全部',
-      images: [] // 素材列表
+      images: [], // 素材列表
+      dialogVisible: false // 弹出添加素材框
     }
   },
   computed: {},
@@ -70,4 +105,7 @@ export default {
 </script>
 
 <style lang="less">
+.v-modal{
+  display: none;
+}
 </style>
