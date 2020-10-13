ce00a51be1297ebc74350c616f0cff85a1087e91
月赛 <1627930319@qq.com>
2020-10-13 20:22:35 +0800

    素材管理-删除收藏图标处理

--- src/views/image/index.vue
+++ src/views/image/index.vue
@@ -27,23 +27,6 @@
       </div>
       <!--/ 按钮组 -->
 
-      <!-- 添加素材 -->
-      <el-dialog title="上传素材" :visible.sync="dialogVisible" width="40%">
-        <el-upload
-          class="upload-demo"
-          drag
-          action="https://jsonplaceholder.typicode.com/posts/"
-          multiple
-        >
-          <i class="el-icon-upload"></i>
-          <div class="el-upload__text">将文件拖到此处，或<em>点击上传</em></div>
-          <div class="el-upload__tip" slot="tip">
-            只能上传jpg/png文件，且不超过500kb
-          </div>
-        </el-upload>
-      </el-dialog>
-      <!--/ 添加素材 -->
-
       <!-- 素材列表 -->
       <el-row :gutter="5">
         <el-col
@@ -53,17 +36,53 @@
           :md="6"
           v-for="(img, index) in images"
           :key="index"
+          class="image-item"
         >
           <el-image style="height: 100px" :src="img.url" fit="cover"></el-image>
+
+          <!-- 收藏和删除 -->
+          <div class="image-action">
+            <i class="el-icon-star-on"></i>
+            <i class="el-icon-delete-solid"></i>
+          </div>
+          <!--/ 收藏和删除 -->
+
         </el-col>
       </el-row>
       <!--/ 素材列表 -->
 
       <!-- 分页 -->
       <div class="block">
-        <el-pagination layout="prev, pager, next"> </el-pagination>
+        <el-pagination
+          layout="prev, pager, next"
+          :total="50"
+          @current-change="onPageChange"
+          :page-size="pageSize"
+          :current-page-sync="page"
+        >
+        </el-pagination>
       </div>
       <!--/ 分页 -->
+
+      <!-- 添加素材 -->
+      <el-dialog title="上传素材" :visible.sync="dialogVisible" width="60%">
+        <el-upload
+          class="upload-demo"
+          drag
+          action="http://ttapi.research.itcast.cn/mp/v1_0/user/images"
+          multiple
+          :headers="uploadHeaders"
+          name="image"
+          :on-success="onUploadSuccess"
+        >
+          <i class="el-icon-upload"></i>
+          <div class="el-upload__text">将文件拖到此处，或<em>点击上传</em></div>
+          <div class="el-upload__tip" slot="tip">
+            只能上传jpg/png文件，且不超过500kb
+          </div>
+        </el-upload>
+      </el-dialog>
+      <!--/ 添加素材 -->
     </el-card>
   </div>
 </template>
@@ -76,36 +95,80 @@ export default {
   components: {},
   props: {},
   data () {
+    const user = JSON.parse(window.localStorage.getItem('user'))
     return {
       radio1: '全部',
       images: [], // 素材列表
-      dialogVisible: false // 弹出添加素材框
+      dialogVisible: false, // 弹出添加素材框
+      uploadHeaders: {
+        Authorization: `Bearer ${user.token}`
+      },
+      page: 1, // 当前页数
+      pageSize: 12, // 当前页面显示图片个数
+      totalCount: 0, // 当前图片总数
+      collect: false // 默认查询全部素材列表
     }
   },
   computed: {},
   watch: {},
   created () {
-    this.loadImages(false)
+    this.loadImages(1, false)
   },
   mounted () { },
   methods: {
-    loadImages (collect) {
+    loadImages (page = 1) {
+      // 重置高亮页码， 防止页面页码不对应
       getImage({
-        collect
+        collect: this.collect,
+        page,
+        per_page: this.pageSize
       }).then(res => {
         this.images = res.data.data.results
+        this.totalCount = res.data.data.total_count
       })
     },
 
     onCollectChange (value) {
-      this.loadImages(value)
+      this.loadImages(1, value)
+    },
+
+    onUploadSuccess () {
+      this.dialogVisible = false
+
+      // 更新素材列表
+      this.loadImages(this.page)
+
+      this.$message({
+        type: 'success',
+        message: '上传成功'
+      })
+    },
+
+    onPageChange (page) {
+      this.loadImages(page)
     }
   }
 }
 </script>
 
 <style lang="less">
-.v-modal{
+.v-modal {
   display: none;
+}
+.image-action {
+  position: absolute;
+  bottom: 4px;
+  right: 5px;
+  left: 5px;
+  height: 40px;
+  background-color: rgba(204, 204, 204, .5);
+  display: flex;
+  font-size: 25px;
+  justify-content: space-around;
+  align-items: center;
+  color: #fff;
+}
+.image-item{
+  position: relative;
 }
 </style>
